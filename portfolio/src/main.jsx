85cf2b5c424321119a8abd39ba6ebd4a5fad63c8
Sarthak_Gupta <sarthak75220@gmail.com>
2025-08-11 09:44:26 +0530

    a

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from '../src/App.jsx'
-import { BrowserRouter } from "react-router"
+import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
