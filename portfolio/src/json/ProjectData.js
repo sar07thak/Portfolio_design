ee83e116019ad8e49749a0277a5a60c9821cfc76
Sarthak Gupta <153414223+sar07thak@users.noreply.github.com>
2026-06-12 23:08:34 +0530

    expense tracker link updated

--- portfolio/src/json/ProjectData.js
+++ portfolio/src/json/ProjectData.js
@@ -46,7 +46,7 @@ const projects = [
   {
     name: "Expense Tracker",
     image: expenseImage,
-    WorkingUrl: "https://expensetracker-front-rpp5.onrender.com/",
+    WorkingUrl: "https://expense-tracker-six-psi-69.vercel.app/login",
     githubUrl: "https://github.com/sar07thak/ExpenseTracker",
     type: "MERN",
     year: 2025,
